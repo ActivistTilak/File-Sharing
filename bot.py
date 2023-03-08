3798c332581b287d1d7a43d4eb79d9ce5a0e3265
ActivistTilak <125390274+ActivistTilak@users.noreply.github.com>
2023-03-08 09:41:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -88,7 +88,7 @@ async def _help(bot, update):
 
 
 async def __reply(update, copied):
-    msg_id = copied.message.id
+    msg_id = copied.id
     if copied.video:
         unique_idx = copied.video.file_unique_id
     elif copied.photo:
